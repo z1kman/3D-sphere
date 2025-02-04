c8e07dbf83eca9bf6bd7a1232f38ccf6c4bc9e8c
Anton Shashkov <z1kmanfast@gmail.com>
2025-02-04 21:41:07 +0400

    basic render added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import type { Component } from "solid-js";
+import { onMount, type Component } from "solid-js";
 import styles from "./App.module.css";
+import { Drawer } from "./Classes/drawer";
 
 const App: Component = () => {
+  let surfaceRef: HTMLCanvasElement | undefined;
+
+  onMount(() => {
+    if (!surfaceRef) return;
+    const ctx = surfaceRef.getContext("2d");
+    const offset = 10; // 5px = App padding (1px * 2) + canvas border (4px * 2)
+    const dpr = window.devicePixelRatio || 1;
+    const width = window.innerWidth - surfaceRef.offsetLeft;
+    const height = window.innerHeight - surfaceRef.offsetTop;
+    surfaceRef.width = width;
+    surfaceRef.height = height;
+    if (!ctx) return;
+    const drawer = new Drawer(ctx, width, height);
+    drawer.addLine({
+      line1: { x: 20, y: 20 },
+      line2: { x: 40, y: 30 },
+      color: { r: 244, g: 10, b: 150 },
+    });
+    drawer.draw();
+    console.log("width, ", width, height);
+  });
   return (
     <div class={styles.App}>
-      <canvas class={styles.Surface}></canvas>
+      <canvas ref={surfaceRef} class={styles.Surface}></canvas>
     </div>
   );
 };
---
+++ src/Classes/drawer.ts
@@ -0,0 +1,84 @@
+type Position = {
+  x: number;
+  y: number;
+};
+
+type Color = {
+  r: number;
+  g: number;
+  b: number;
+};
+export class Drawer {
+  ctx: CanvasRenderingContext2D | null = null;
+  width: number = 0;
+  height: number = 0;
+  surface: ImageData | null;
+
+  constructor(ctx: CanvasRenderingContext2D, width: number, height: number) {
+    this.ctx = ctx;
+    this.width = width;
+    this.height = height;
+    this.surface = ctx.getImageData(0, 0, width, height);
+  }
+
+  draw() {
+    if (!this.surface || !this.ctx) return;
+
+    this.ctx.putImageData(this.surface, 0, 0);
+  }
+
+  addPixel(position: Position, color: Color) {
+    if (!this.surface) return;
+    const { x, y } = position;
+    const { r, g, b } = color;
+    const index = (this.width * y + x) * 4;
+    const surface = this.surface;
+
+    surface.data[index] = r;
+    surface.data[index + 1] = g;
+    surface.data[index + 2] = b;
+    surface.data[index + 3] = 255;
+  }
+
+  addLine({
+    line1,
+    line2,
+    color,
+  }: {
+    line1: Position;
+    line2: Position;
+    color: Color;
+  }) {
+    const { r, g, b } = color;
+    const c1 = line2.y - line1.y;
+    const c2 = line2.x - line1.x;
+
+    const length = Math.sqrt(c1 * c1 + c2 * c2);
+
+    const xStep = c2 / length;
+    const yStep = c1 / length;
+
+    for (let i = 0; i < length; i++) {
+      this.addPixel(
+        {
+          x: Math.trunc(line1.x + xStep * i),
+          y: Math.trunc(line1.y + yStep * i),
+        },
+        {
+          r,
+          g,
+          b,
+        }
+      );
+    }
+  }
+
+  clearSurface() {
+    if (!this.surface) return;
+    const surfaceSize = this.width * this.height * 4;
+    for (let i = 0; i < surfaceSize; i++) {
+      if (!this.surface.data[i]) continue;
+      this.surface.data[i] = 0;
+    }
+  }
+}
