4031032c37f58e803e10cc7c51625059f594dddf
Anton Shashkov <z1kmanfast@gmail.com>
2025-02-04 20:48:39 +0400

    Surface added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import type { Component } from 'solid-js';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
+import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
+      <canvas class={styles.Surface}></canvas>
     </div>
   );
 };
